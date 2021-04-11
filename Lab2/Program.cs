caf165ffd19006c6d819ccab58129013839e9485
Iulia Chebykina <julia.chebykina.13@gmail.com>
2021-04-11 20:41:02 +0500

    Chebykina Iulia

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Xml;
-using static System.Int32;
 using static System.String;
 
 namespace Lab2
@@ -13,126 +12,82 @@ namespace Lab2
 
         public static void Main(string[] args)
         {
-            var repeat = 0;
-            var startLength = 0;
-            var maxLength = 0;
-            var difference = 0;
-            var denominator = 0.0;
-            var minElement = 0;
-            var maxElement = 0;
+            Console.WriteLine("Введите адрес xml-файла с планом эксперимента: " );
+            var expPlanPath = Console.ReadLine();
             var xDoc = new XmlDocument();
             try
             {
-                xDoc.Load("C:/Users/cheby/RiderProjects/Lab2/Lab2/Experiment.xml");
+                xDoc.Load(expPlanPath ?? Empty);
             }
             catch
             {
-                throw new ArgumentException("xml файл не найден");
+                Console.WriteLine("xml файл не найден");
+                Console.ReadLine();
+                return;
             }
-
             var xRoot = xDoc.DocumentElement;
-            if (xRoot == null) throw new ArgumentException("В xml файле не найдено элементов");
-            var childArith = xRoot.ChildNodes[0].Name == "arithmeticProgression"
-                ? xRoot.ChildNodes[0].ChildNodes
-                : xRoot.ChildNodes[1].ChildNodes;
-            var childGeom = xRoot.ChildNodes[0].Name == "arithmeticProgression"
-                ? xRoot.ChildNodes[1].ChildNodes
-                : xRoot.ChildNodes[0].ChildNodes;
-            foreach (XmlNode childNode in childArith)
+            Console.WriteLine("Введите адрес текстового файла для вывода результатов: ");
+            var resPath = Console.ReadLine();
+            if (xRoot != null)
             {
-                switch (childNode.Name)
+                var experiments = xRoot.SelectNodes("experiment")?.Item(0)?.ChildNodes;
+                using (var streamWriter = new StreamWriter(resPath ?? Empty))
                 {
-                    case "repeat":
-                        repeat = Parse(childNode.InnerText);
-                        break;
-                    case "startLength":
-                        startLength = Parse(childNode.InnerText);
-                        break;
-                    case "maxLength":
-                        maxLength = Parse(childNode.InnerText);
-                        break;
-                    case "minElement":
-                        minElement = 70 + Parse(childNode.InnerText);
-                        break;
-                    case "maxElement":
-                        maxElement = 70 + Parse(childNode.InnerText);
-                        break;
-                    case "difference":
-                        difference = Parse(childNode.InnerText);
-                        break;
+                    if (experiments != null)
+                        foreach (XmlNode experiment in experiments)
+                        {
+                            if (experiment.Attributes != null &&
+                                experiment.Attributes["name"].Value == "arithmetic progression")
+                            {
+                                var repeat = Int32.Parse(experiment.Attributes["repeat"].Value);
+                                var startLength = Int32.Parse(experiment.Attributes["startLength"].Value);
+                                var maxLength = Int32.Parse(experiment.Attributes["maxLength"].Value);
+                                var difference = Int32.Parse(experiment.Attributes["difference"].Value);
+                                var minElement = Int32.Parse(experiment.Attributes["minElement"].Value);
+                                var maxElement = Int32.Parse(experiment.Attributes["maxElement"].Value);
+                                while (startLength <= maxLength)
+                                {
+                                    for (var i = 0; i < repeat; i++)
+                                    {
+                                        var text = GetString(startLength, minElement, maxElement).ToCharArray();
+                                        var rbtSorting = new RbtSort<char>();
+                                        rbtSorting.Sort(text);
+                                        var count = rbtSorting.GetCount();
+                                        streamWriter.WriteLine(text.Length + " " + count);
+                                    }
+
+                                    startLength += difference;
+                                }
+                            }
+                            else if (experiment.Attributes != null &&
+                                     experiment.Attributes["name"].Value == "geometric progression")
+                            {
+                                var repeat = Int32.Parse(experiment.Attributes["repeat"].Value);
+                                var startLength = Int32.Parse(experiment.Attributes["startLength"].Value);
+                                var maxLength = Int32.Parse(experiment.Attributes["maxLength"].Value);
+                                var denominator = Double.Parse(experiment.Attributes["denominator"].Value);
+                                var minElement = Int32.Parse(experiment.Attributes["minElement"].Value);
+                                var maxElement = Int32.Parse(experiment.Attributes["maxElement"].Value);
+                                while (startLength <= maxLength)
+                                {
+                                    for (var i = 0; i < repeat; i++)
+                                    {
+                                        var text = GetString(startLength, minElement, maxElement).ToCharArray();
+                                        var rbtSorting = new RbtSort<char>();
+                                        rbtSorting.Sort(text);
+                                        var count = rbtSorting.GetCount();
+                                        streamWriter.WriteLine(text.Length + " " + count);
+                                    }
+
+                                    startLength = (int) (startLength * denominator);
+                                }
+                            }
+                        }
                 }
             }
 
-            var arithmeticProgression = new List<Tuple<int, int>>();
-            while (startLength <= maxLength)
-            {
-                for (var i = 0; i < repeat; i++)
-                {
-                    var text = GetString(startLength, minElement, maxElement).ToCharArray();
-                    var rbtSorting = new RbtSort<char>();
-
-                    rbtSorting.Sort(text);
-                    var count = rbtSorting.GetCount();
-                    var tuple = Tuple.Create(text.Length, count);
-
-                    arithmeticProgression.Add(tuple);
-                }
-
-                startLength += difference;
-            }
-
-            var sw = new StreamWriter("result.txt");
-            foreach (var (item1, item2) in arithmeticProgression)
-            {
-                sw.WriteLine(item1 + " " + item2);
-            }
-
-            foreach (XmlNode childNode in childGeom)
-            {
-                switch (childNode.Name)
-                {
-                    case "repeat":
-                        repeat = Parse(childNode.InnerText);
-                        break;
-                    case "startLength":
-                        startLength = Parse(childNode.InnerText);
-                        break;
-                    case "maxLength":
-                        maxLength = Parse(childNode.InnerText);
-                        break;
-                    case "minElement":
-                        minElement = 70 + Parse(childNode.InnerText);
-                        break;
-                    case "maxElement":
-                        maxElement = 70 + Parse(childNode.InnerText);
-                        break;
-                    case "denominator":
-                        denominator = Convert.ToDouble(childNode.InnerText);
-                        break;
-                }
-            }
-
-            var geometricProgression = new List<Tuple<int, int>>();
-            while (startLength <= maxLength)
-            {
-                for (var i = 0; i < repeat; i++)
-                {
-                    var text = GetString(startLength, minElement, maxElement).ToCharArray();
-                    var rbtSorting = new RbtSort<char>();
-                    rbtSorting.Sort(text);
-                    var count = rbtSorting.GetCount();
-                    var tuple = Tuple.Create(text.Length, count);
-                    geometricProgression.Add(tuple);
-                }
-
-                startLength = (int) (startLength * denominator);
-            }
-
-            foreach (var (item1, item2) in geometricProgression)
-            {
-                sw.WriteLine(item1 + "\t" + item2);
-            }
-            sw.Close();
+            Console.WriteLine("Эксперимент проведен успешно. Результаты в указанном вами файле");
+            Console.ReadLine();
         }
 
         private static string GetString(int length, int minElement, int maxElement)
